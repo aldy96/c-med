2c5d2351137390f6ae2a364c9a0fc7468f80a4d5
Aldy Fernando Sobandi <47637558+aldy96@users.noreply.github.com>
2024-07-15 19:22:52 +0700

    baru

---
+++ server.js
@@ -0,0 +1,25 @@
+/*
+  Include express and passport packages.
+*/
+const express = require('express');
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const app = express();
+
+
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+app.use(bodyParser.json())
+/*
+  Database connection -- We are using MongoDB for this tutorial
+*/
+require('./passport/passport')(app)
+
+require('./routes/auth')(app)
+
+app.listen(5000, () => { console.log('Server started.') });
